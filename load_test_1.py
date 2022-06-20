f6dcf451df9a5b00c48c9e692a42ea5966fa810a
chloehsu170 <xuyanwen170@gmail.com>
2022-06-20 14:49:30 -0500

    delete comments

--- load_test_1.py
+++ load_test_1.py
@@ -9,7 +9,7 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 class MyBlogs(TaskSet):
     @task(1)
     def get_blog(self):
-        
+
         header = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"}
 
@@ -23,8 +23,8 @@ class MyBlogs(TaskSet):
 
 class websitUser(HttpLocust):
     task_set = MyBlogs
-    min_wait = 3000  # 单位为毫秒
-    max_wait = 6000  # 单位为毫秒
+    min_wait = 3000
+    max_wait = 6000
 
 
 if __name__ == "__main__":
