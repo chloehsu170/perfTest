60db03ed9c275618bd1794f86991e89c9e04b11b
Yanwen-Xu <Yanwen.Xu@coxautoinc.com>
2022-06-20 14:37:03 -0500

    delete some comment

--- load_test_1.py
+++ load_test_1.py
@@ -2,15 +2,14 @@
 import requests
 from locust import HttpLocust, TaskSet, task
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-# 禁用安全请求警告
+
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
 class MyBlogs(TaskSet):
-    # 访问我的博客首页
     @task(1)
     def get_blog(self):
-        # 定义请求头
+        
         header = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"}
 
